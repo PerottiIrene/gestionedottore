5835d8d0fd2dddbd2ab998458e74821888150c03
PerottiIrene <ireneperotti29@gmail.com>
2022-12-01 20:32:57 +0100

    creata classe di model

---
+++ src/main/java/it/prova/gestionedottore/model/Dottore.java
@@ -0,0 +1,104 @@
+package it.prova.gestionedottore.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dottore")
+public class Dottore {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "codiceDottore")
+	private String codiceDottore;
+	@Column(name = "codiceFiscalePazienteAttualmenteInVisita")
+	private String codiceFiscalePazienteAttualmenteInVisita;
+	@Column(name = "inVisita")
+	private boolean inVisita;
+	@Column(name = "inServizio")
+	private boolean inServizio;
+	
+	public Dottore() {}
+
+	public Dottore(Long id, String nome, String cognome, String codiceDottore,
+			String codiceFiscalePazienteAttualmenteInVisita, boolean inVisita, boolean inServizio) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceDottore = codiceDottore;
+		this.codiceFiscalePazienteAttualmenteInVisita = codiceFiscalePazienteAttualmenteInVisita;
+		this.inVisita = inVisita;
+		this.inServizio = inServizio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceDottore() {
+		return codiceDottore;
+	}
+
+	public void setCodiceDottore(String codiceDottore) {
+		this.codiceDottore = codiceDottore;
+	}
+
+	public String getCodiceFiscalePazienteAttualmenteInVisita() {
+		return codiceFiscalePazienteAttualmenteInVisita;
+	}
+
+	public void setCodiceFiscalePazienteAttualmenteInVisita(String codiceFiscalePazienteAttualmenteInVisita) {
+		this.codiceFiscalePazienteAttualmenteInVisita = codiceFiscalePazienteAttualmenteInVisita;
+	}
+
+	public boolean isInVisita() {
+		return inVisita;
+	}
+
+	public void setInVisita(boolean inVisita) {
+		this.inVisita = inVisita;
+	}
+
+	public boolean isInServizio() {
+		return inServizio;
+	}
+
+	public void setInServizio(boolean inServizio) {
+		this.inServizio = inServizio;
+	}
+	
+	
+}
