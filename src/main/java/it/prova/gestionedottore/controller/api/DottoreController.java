bf1748943e26ffcfc85e492e1c9ec67c76f8b887
PerottiIrene <ireneperotti29@gmail.com>
2022-12-01 22:08:48 +0100

    terminato crud dottore

--- src/main/java/it/prova/gestionedottore/controller/api/DottoreController.java
+++ src/main/java/it/prova/gestionedottore/controller/api/DottoreController.java
@@ -2,11 +2,15 @@ package it.prova.gestionedottore.controller.api;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,20 +20,18 @@ import it.prova.gestionedottore.dto.DottoreDTO;
 import it.prova.gestionedottore.model.Dottore;
 import it.prova.gestionedottore.service.DottoreService;
 
-
 @RestController
 @RequestMapping("/api/dottore")
 public class DottoreController {
-	
+
 	@Autowired
 	private DottoreService dottoreService;
-	
+
 	@GetMapping("/{id}")
 	public DottoreDTO findById(@PathVariable(value = "cf", required = true) Long id) {
 		Dottore result = dottoreService.caricaSingoloElemento(id);
-		//qui andrebbe gestito con un 404 ma per semplicità mandiamo un oggetto vuoto
-		return result == null ? new DottoreDTO()
-				: DottoreDTO.buildDottoreDTOFromModel(result);
+		// qui andrebbe gestito con un 404 ma per semplicità mandiamo un oggetto vuoto
+		return result == null ? new DottoreDTO() : DottoreDTO.buildDottoreDTOFromModel(result);
 	}
 
 	@GetMapping
@@ -48,6 +50,40 @@ public class DottoreController {
 
 		Dottore result = dottoreService.inserisciNuovo(input.buildDottoreModel());
 		return DottoreDTO.buildDottoreDTOFromModel(result);
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public DottoreDTO update(@Valid @RequestBody DottoreDTO input, @PathVariable(required = true)  Long id) {
+
+		Dottore dottore = dottoreService.caricaSingoloElemento(id);
+		// ANDREBBE GESTITA CON ADVICE!!!
+		// se mi viene inviato un id jpa lo interpreta come update ed a me (producer)
+		// non sta bene
+		if (dottore.getId() == null)
+			throw new RuntimeException("Dottore not found con id: " + id);
+
+		input.setId(id);
+		Dottore dottoreAggiornato = dottoreService.aggiorna(input.buildDottoreModel());
+		return DottoreDTO.buildDottoreDTOFromModel(dottoreAggiornato);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(required = true) Long id) {
+
+		Dottore dottore = dottoreService.caricaSingoloElemento(id);
+
+		if (dottore.getId() == null)
+			throw new RuntimeException("Dottore not found con id: " + id);
+
+		if (dottore.isInServizio())
+			throw new RuntimeException("Impossibile eliminare il dottore, e' in servizio");
+
+		if (dottore.isInVisita())
+			throw new RuntimeException("Impossibile eliminare il dottore, e' in visita");
+
+		dottoreService.rimuovi(id);
 	}
 
 }
